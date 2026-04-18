7c55b1e02611078c130bd381a1ec7c337be34bf1
Timothy Gregg <timothy.gregg@complete.tech>
2026-04-18 00:45:51 -0400

    Fix timeline state SaveManager import

--- McpMod.StateBuilder.cs
+++ McpMod.StateBuilder.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MegaCrit.Sts2.Core.Saves;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Context;
 using MegaCrit.Sts2.Core.Entities.Cards;
